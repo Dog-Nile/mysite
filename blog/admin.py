28716e6ace3481256eb4660e0bab67ae486f1215
imhyuk-huno <imhyuk@huno.kr>
2024-02-14 18:52:37 +0900

    feat: blog app is added

---
+++ blog/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'modified_at')
+    list_filter = ('modified_at',)
+    search_fields = ('title', 'content')
+    prepopulated_fields = {"slug": ('title',)}
+
+
+
+
